f866d320983f080af07ed764ae40030c278ac84e
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-25 11:42:10 +0200

    adds watcher config

--- flake.nix
+++ flake.nix
@@ -6,11 +6,46 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, utils, ... }:
-    utils.lib.eachDefaultSystem (_: {
+  outputs = { self, utils, nixpkgs, ... }:
+    utils.lib.eachDefaultSystem (system: {
         # Expose the NixOS module for flake users
         nixosModules = { 
           linkman = ./nix/linkman.nix;
         };
+
+        devShells = let
+          pkgs = import nixpkgs { inherit system; };
+
+          configFile = pkgs.writeTextFile {
+            name = ".watch.yml";
+            text = pkgs.lib.generators.toYAML { } [
+              {
+                name = "linkman";
+                run = [
+                  "nix flake check"
+                ];
+                change = [ "**/*.nix" ];
+              }
+              {
+                name = "git stage changed files";
+                run = [
+                  "git add {{relative_path}}"
+                ];
+                change = [ "**/*.nix" ];
+              }
+            ];
+          };
+        in {
+          default = pkgs.mkShell {
+            name = "linkman-shell";
+            buildInputs = [
+              pkgs.funzzy
+            ];
+
+            shellHook = ''
+              ln -sf ${configFile} .watch.yml
+            '';
+          };
+        }; 
       });
 }
